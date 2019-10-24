c385551246dc0ce88800f3f06f8ffe1ed82c0560
JeanBaptisteServais <54853371+JeanBaptisteServais@users.noreply.github.com>
2019-10-24 18:10:02 +0200

    Add files via upload

---
+++ paths.py
@@ -0,0 +1,16 @@
+#path of current treating image
+path_picture_current = "dataset/image/current/current.jpg"
+path_folder_current = "dataset/image/current/"
+
+#path of label file
+path_label_py = "dataset/information_data/label.py"
+
+#path of picture/dataset
+path_dataset = "dataset/image/dataset"
+path_folder_folder = "dataset/image/dataset/{}"
+path_image = "dataset/image/dataset/{}/{}"
+
+#path for training (csv, model)
+path_folder_models = "training/models/models/"
+path_csv_name = "training/csv/in_training/"
+path_model_name = "training/models/in_training/"
