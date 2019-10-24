4e45a214cc8cd7128c32a9da84637f5fba898e99
JeanBaptisteServais <54853371+JeanBaptisteServais@users.noreply.github.com>
2019-10-24 22:41:44 +0200

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,6 +1,9 @@
 from step_one import step_one
 
-
+path_folder_current = "../dataset/image/current/"
+path_picture = "../dataset/image/current/current.jpg"
+path_position = "../dataset/information_data/current/position.py"
 
 if __name__ == "__main__":
-    step_one()
+    
+    step_one(path_folder_current, path_picture, path_position)
