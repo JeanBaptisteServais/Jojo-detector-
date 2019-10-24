8a25f281ba49ab5226469a1ffc6f8816159b8f73
JeanBaptisteServais <54853371+JeanBaptisteServais@users.noreply.github.com>
2019-10-24 22:50:38 +0200

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,6 +1,7 @@
 import sys
-from step_one import step_one
 sys.path.append(r"C:\Users\jeanbaptiste\Desktop\assiette\v2")
+from step_one import step_one
+
 
 path_folder_current = "../dataset/image/current/"
 path_picture = "../dataset/image/current/current.jpg"
