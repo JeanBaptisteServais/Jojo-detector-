275155bf839845d8e4f333262aefaac7ec3da70a
JeanBaptisteServais <54853371+JeanBaptisteServais@users.noreply.github.com>
2019-10-25 00:10:28 +0200

    Update step_two.py

--- main/step_two.py
+++ main/step_two.py
@@ -74,6 +74,34 @@ def picture_treatment(image, nb, path_current, path_copy):
 
 
 
+def draw_function1(img, j, width, i, height):
+    """We verify if there are background pixels"""
+
+    bg_pix = 0;
+
+    for jj in range(j, j + width):
+        for ii in range(i, i + height): 
+            if img[jj, ii][0] != 177 and\
+               img[jj, ii][1] != 151 and\
+               img[jj, ii][2] != 151:
+                bg_pix += 1
+
+    return bg_pix
+
+
+def draw_function2(img, img1, i, height, j, width, x, y, name):
+    """We put the crop picture on the display picture"""
+
+    font = cv2.FONT_HERSHEY_COMPLEX_SMALL;
+    img[j:j + width, i:i + height] = img1
+    cv2.line(img, (i + height, j+ width),
+             (x + 200, y + 200), (0, 0, 0), 2)
+    cv2.putText(img, name, (i,j+width),
+                font, 1, (0, 0, 255))
+
+    return img
+
+
 def draw(detection, nb, image, path_current, path_copy):
     """We add border to picture, We course the picture,
     if the course is the picture, we passe. In some,
@@ -86,23 +114,37 @@ def draw(detection, nb, image, path_current, path_copy):
         for i in range(0, img.shape[1], img1.shape[1]):
             if j >= 100 and j <= 380 and i >= 100 and i <= 450 : pass
             else:
-                bg_pix = 0
-                for jj in range(j, j + img1.shape[0]):
-                    for ii in range(i, i + img1.shape[1]): 
-                        if img[jj, ii][0] != 177 and\
-                           img[jj, ii][1] != 151 and\
-                           img[jj, ii][2] != 151:
-                            bg_pix += 1
+                bg_pix = draw_function1(img, j, img1.shape[0], i, img1.shape[1])
 
                 if bg_pix == 0:
 
-                    img[j:j + img1.shape[0], i:i + img1.shape[1]] = img1
-                    cv2.line(img, (i+ img1.shape[1], j+ img1.shape[0]),
-                             (x + 200, y + 200), (0, 0, 0), 2)
-                    cv2.putText(img, name, (i,j+img1.shape[0]),
-                                cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255))
+                    img = draw_function2(img, img1, i, img1.shape[1],
+                                         j, img1.shape[0], x, y, name)
 
                     return img
+
+
+
+def predictions(labels, images, detections, models, path_models, image, img):
+    """Try to find a detection from the label"""
+
+    for lab in labels:
+        _, w, h, l , n = treatment_read([lab])
+
+        try:
+            model = path_models + str(models)
+            prediction = detection(model, w, h, img)
+        except:
+            pass
+
+        if prediction == l:
+            detections.append([n, recup_position(image)])
+            images.append(image)
+            break
+        else:
+            detections.append(["", recup_position(image)])
+            images.append(image)
+            break
 
 
 #detection, label
@@ -125,24 +167,8 @@ def step_two(path_current, path_copy,
 
         for models in model_list:
             labels = read(path_label, str(models))
-
-            for lab in labels:
-                _, w, h, l , n = treatment_read([lab])
-
-                try:
-                    model = path_models + str(models)
-                    prediction = detection(model, w, h, img)
-                except:
-                    pass
-
-                if prediction == l:
-                    detections.append([n, recup_position(image)])
-                    images.append(image)
-                    break
-                else:
-                    detections.append(["", recup_position(image)])
-                    images.append(image)
-                    break
+            predictions(labels, images, detections, models,
+                        path_models, image, img)
 
     for nb, i in enumerate(detections):
 
