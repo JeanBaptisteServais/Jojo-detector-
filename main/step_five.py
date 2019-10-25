5071d83146e73dd76bd36e998e20ed35d1d5dcc6
JeanBaptisteServais <54853371+JeanBaptisteServais@users.noreply.github.com>
2019-10-25 17:08:56 +0200

    Update step_five.py

--- main/step_five.py
+++ main/step_five.py
@@ -11,6 +11,11 @@ import importlib
 
 
 def step_five(path_data):
+    """Here we want recup dimension of pictures.
+    For that we go on the dataset picture folder.
+    We run all of the picture folder,
+    recup x and y pixels convert to cm.
+    For that we call write who run a thread"""
 
     liste_path = os.listdir(path_data)
 
@@ -23,9 +28,3 @@ def step_five(path_data):
 
 
     return liste
-
-
-
-
-
-step_five(path_data)
