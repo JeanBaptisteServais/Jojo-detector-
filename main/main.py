c32d5fc06026ad8b0abc6cfc0ee76cd95c94eee0
JeanBaptisteServais <54853371+JeanBaptisteServais@users.noreply.github.com>
2019-10-25 17:35:14 +0200

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,10 +1,14 @@
 import sys
 sys.path.append(r"C:\Users\jeanbaptiste\Desktop\assiette\v2")
+
 from step_one import step_one
 from step_two import step_two
 from step_three import step_three
 from step_fourth import step_fourth
 from step_five import step_five
+from step_six import step_six
+from step_seven import step_seven
+
 
 path_folder_current = "../dataset/image/current/"
 path_picture = "../dataset/image/current/current.jpg"
@@ -16,7 +20,8 @@ path_current = "../dataset/image/current/current.jpg"
 path_data = "../dataset/image/dataset"
 path_folder_format = "../dataset/image/dataset/{}"
 path_image_format = "../dataset/image/dataset/{}/{}"
-
+path_csv_name = "training/csv/in_training/{}.csv"
+path_model_name = "training/models/in_training/{}"
 
 
 
@@ -32,9 +37,29 @@ if __name__ == "__main__":
 
     #detections = step_two(path_current, path_copy, path_folder_current,
     #                      path_models, path_label)
+
     #found = step_three(detections, path_folder_current)
+
     #step_fourth(objects, path_data, path_folder_format, path_image_format)
-    #step_five(path_data)
-    #step_six()
+
+    #dimensions = step_five(path_data)
+
+##    step_six(dimensions, path_data, path_folder_format,
+##             path_image_format, path_label, path_csv_name,
+##             path_model_name)
+
     #a, b = step_seven()
     #if a: liste.append(b)
+
+
+
+
+
+
+
+
+
+
+
+
+    
