80ba7aa0a85ac7eb3c1f77da512c115ca9bbd216
JeanBaptisteServais <54853371+JeanBaptisteServais@users.noreply.github.com>
2019-10-25 18:03:06 +0200

    Update step_seven.py

--- main/step_seven.py
+++ main/step_seven.py
@@ -1,55 +1,43 @@
-def step_seven():
+import os
+from main_function_image import open_picture
+from main_function_image import show_picture
+from object_detection.objects_detection import detection
+from dataset.information_data.labels_function import read
+from dataset.information_data.labels_function import treatment_read
 
-    print("Detection in progress ...")
 
-    path_current = "dataset/image/current/"
-    path_models = "training/models/in_training/"
-    path_label = "dataset/information_data/label.py"
+def step_seven(path_current, path_models, path_label):
 
     liste_picture = os.listdir(path_current)
     model_list = os.listdir(path_models)
-
-    detections = []
-    images = []
+    detections = []; images = [];
+    liste_picture.remove("current.jpg")
+    liste_picture.remove("current_copy.jpg")
 
 
     for picture in liste_picture:
+        img = open_picture(path_current + str(picture))
 
-        if picture != "current.jpg" and\
-           picture != "current_copy.jpg":
+        for models in model_list:
+            model = path_models + str(models)
+            labels = read(path_label, str(None))
 
-            image = path_current + str(picture)
+            for lab in labels:
+                information, w, h = treatment_read([lab])
 
-            img = open_picture(image)
-
-            for models in model_list:
-
-                model = path_models + str(models)
-
-                labels = read(path_label, str(None))
-
-
-                for lab in labels:
-                    information = treatment_read([lab])
-
-                    w = int(information["dimension"][0])
-                    h = int(information["dimension"][1])
-
-                    try:
-                        prediction = detection(model, w, h, img)
-                        print(models, prediction)
-                    except:
-                        pass
+                try:
+                    prediction = detection(model, w, h, img)
+                    print(models, prediction)
 
                     if prediction == 1:
                         detections.append([information["name"], models])
                         images.append(image)
 
-                    else:
-                        pass
+                except ValueError:
+                    pass
 
-
-                    #show_picture("picture", img, 1, "y")
+                show_picture("picture", img, 0, "y")
 
 
     print(detections)
+    return detections
