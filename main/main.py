2703633a98fa9f4b585196715f6062c2287c0d8b
JeanBaptisteServais <54853371+JeanBaptisteServais@users.noreply.github.com>
2019-10-25 17:03:37 +0200

    Update main.py

--- main/main.py
+++ main/main.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append(r"C:\Users\jeanbaptiste\Desktop\assiette\v2")
 from step_one import step_one
 from step_two import step_two
+from step_three import step_three
 
 path_folder_current = "../dataset/image/current/"
 path_picture = "../dataset/image/current/current.jpg"
@@ -10,6 +11,10 @@ path_models = "../training/models/models/"
 path_label = "../dataset/information_data/label.py"
 path_copy = "../dataset/image/current/current_copy.jpg"
 path_current = "../dataset/image/current/current.jpg"
+path_data = "../dataset/image/dataset"
+path_folder_format = "../dataset/image/dataset/{}"
+path_image_format = "../dataset/image/dataset/{}/{}"
+
 
 
 
@@ -17,5 +22,7 @@ path_current = "../dataset/image/current/current.jpg"
 if __name__ == "__main__":
     
     #step_one(path_folder_current, path_picture, path_position)
-    step_two(path_current, path_copy, path_folder_current,
-             path_models, path_label)
+    #detections = step_two(path_current, path_copy, path_folder_current,
+    #                      path_models, path_label)
+    #found = step_three(detections, path_folder_current)
+    #step_fourth(objects, path_data, path_folder_format, path_image_format)
