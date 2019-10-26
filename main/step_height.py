06192a3ad0f48e9d476a974473afffb21ce2ad5c
JeanBaptisteServais <54853371+JeanBaptisteServais@users.noreply.github.com>
2019-10-26 03:27:04 +0200

    Update step_height.py
    oups

--- main/step_height.py
+++ main/step_height.py
@@ -1,87 +1,69 @@
 import sys
 sys.path.append(r"C:\Users\jeanbaptiste\Desktop\assiette\v2")
 
-import os
-import csv
+from step_one import step_one
+from step_two import step_two
+from step_three import step_three
+from step_fourth import step_fourth
+from step_five import step_five
+from step_six import step_six
+from step_seven import step_seven
+from step_height import step_height
 
-from training.training import csv_to_list
-from training.training import training
-from training.training import train
-def last_csv(liste_csv):
+path_folder_current = "../dataset/image/current/"
+path_picture = "../dataset/image/current/current.jpg"
+path_position = "../dataset/information_data/current/position.py"
+path_models = "../training/models/models/"
+path_label = "../dataset/information_data/label.py"
+path_copy = "../dataset/image/current/current_copy.jpg"
+path_current = "../dataset/image/current/current.jpg"
+path_data = "../dataset/image/dataset"
+path_folder_format = "../dataset/image/dataset/{}"
+path_image_format = "../dataset/image/dataset/{}/{}"
+path_csv_name = "../training/csv/in_training/{}.csv"
+path_model_name = "../training/models/in_training/{}"
+path_to_thread = "../ecriture/to_thread.py"
+path_models_training = "../training/models/in_training/"
+path_csv = "../training/csv/csv"
+path_model = "../training/models/models"
+path_csv_training = "../training/csv/in_training"
 
-    liste_w = []
-    for i in liste_csv:
-        if i == '.~lock.2.csv#':pass
-        else:
-            liste_w.append(len(i))
-    maxi = max(liste_w)
+def raise_file_wrote():
+    os.remove(path_to_thread)
 
-    liste_w2 = []
-    for i in liste_csv:
-        if len(i) == maxi:
-            liste_w2.append(i)
-
-    labeled = []
-    to_read = path_csv + "/" + str(max(liste_w2))
-
-    f =  open(to_read, 'r')
-    dataframe = f.readlines()
-    dataframe = dataframe
-    for i in dataframe:
-        try:
-            labeled.append(int(i[0]))
-        except:
-            pass
-
-    labeled = max(list(set(labeled)))
-
-    return labeled, to_read
+if __name__ == "__main__":
 
 
 
+    #liste = []; length = 0;
+    #while len(liste) != length:
+
+    #raise_file_wrote()
+
+    #length = step_one(path_folder_current, path_picture, path_position)
+
+    #detections = step_two(path_current, path_copy, path_folder_current,
+    #                      path_models, path_label)
+
+    #found = step_three(detections, path_folder_current)
+
+    #step_fourth(objects, path_data, path_folder_format, path_image_format)
+
+    #dimensions = step_five(path_data)
+
+##    step_six(dimensions, path_data, path_folder_format,
+##             path_image_format, path_label, path_csv_name,
+##             path_model_name)
+
+    liste = step_seven(path_folder_current, path_models_training, path_label)
+    #liste + b
+
+    step_height(liste, path_csv_training, path_csv,
+                path_model, path_models_training)
 
 
-def step_height(liste, path_csv_training, path_csv,
-                path_model_training, liste_model):
-
-    name = list(set([i[0] for i in liste]))
-
-    liste_training_csv = os.listdir(path_csv_training)
-    liste_csv = os.listdir(path_csv)
-
-    liste_model_training = os.listdir(path_model_training)
-    liste_model = os.listdir(path_model)
-
-    labeled, to_read = last_csv(liste_csv)
-    label = labeled + 1
 
 
-    to_change = []
-    for i in name:
-        for csv in liste_training_csv:
-            if str(i) + ".csv" == str(csv):
-                to_change.append(csv)
-
-    for i in to_change:
-        path = path_csv_training + "/" + str(i)
-        main = open(to_read, 'a')
-
-        f =  open(path, 'r')
-        dataframe = f.readlines()
-        dataframe = dataframe
-
-        for j in dataframe:
-            ok = False
-            if j[0] == "1":
-                j = j.split(";")
-                j[0] = str(label)
-                for k in j[:-1]:
-                    main.write(str(k)+";")
-                ok = True
-            if ok is True:
-                main.write("\n")
-        label += 1
 
 
-    X, y = csv_to_list(to_read)
-    training(X, Y, to_read)
+    
