a71c2f09b925a56a5c24abc314db4ece6a7612aa
JeanBaptisteServais <54853371+JeanBaptisteServais@users.noreply.github.com>
2019-10-26 03:19:18 +0200

    Update main.py

--- main/main.py
+++ main/main.py
@@ -8,7 +8,7 @@ from step_fourth import step_fourth
 from step_five import step_five
 from step_six import step_six
 from step_seven import step_seven
-
+from step_height import step_height
 
 path_folder_current = "../dataset/image/current/"
 path_picture = "../dataset/image/current/current.jpg"
@@ -20,9 +20,13 @@ path_current = "../dataset/image/current/current.jpg"
 path_data = "../dataset/image/dataset"
 path_folder_format = "../dataset/image/dataset/{}"
 path_image_format = "../dataset/image/dataset/{}/{}"
-path_csv_name = "training/csv/in_training/{}.csv"
-path_model_name = "training/models/in_training/{}"
-path_to_thread = "ecriture/to_thread.py"
+path_csv_name = "../training/csv/in_training/{}.csv"
+path_model_name = "../training/models/in_training/{}"
+path_to_thread = "../ecriture/to_thread.py"
+path_models_training = "../training/models/in_training/"
+path_csv = "../training/csv/csv"
+path_model = "../training/models/models"
+path_csv_training = "../training/csv/in_training"
 
 def raise_file_wrote():
     os.remove(path_to_thread)
@@ -31,14 +35,12 @@ if __name__ == "__main__":
 
 
 
-    #liste = []
-    #length = 0
+    #liste = []; length = 0;
     #while len(liste) != length:
 
     #raise_file_wrote()
 
-    #step_one(path_folder_current, path_picture, path_position)
-    #length = l
+    #length = step_one(path_folder_current, path_picture, path_position)
 
     #detections = step_two(path_current, path_copy, path_folder_current,
     #                      path_models, path_label)
@@ -53,14 +55,11 @@ if __name__ == "__main__":
 ##             path_image_format, path_label, path_csv_name,
 ##             path_model_name)
 
-    #a, b = step_seven()
-    #if a: liste.append(b)
+    liste = step_seven(path_folder_current, path_models_training, path_label)
+    #liste + b
 
-
-
-
-
-
+    step_height(liste, path_csv_training, path_csv,
+                path_model, path_model_training)
 
 
 
